0d30b2f1f1e19516ec3095de0bbd1f6f3a313449
desi <moonlight1211@proton.me>
2023-02-15 12:42:15 -0500

    js

--- fitness.js
+++ fitness.js
@@ -1,2 +1,18 @@
 
 /* FITNESS PROJECT JAVASCRIPT FILE */
+const cells = document.querySelectorAll('.tableCell');
+
+for(let cell of cells){
+    cell.addEventListener('click', function(){
+        document.getElementById('inputBox').remove();
+        const div = document.createElement('div');
+        div.id = 'inputBox'
+        const content = generateDivContent(cell);        
+
+        document.querySelector('main').appendChild(div);
+    });
+}
+
+function generateDivContent(cell){
+
+}
